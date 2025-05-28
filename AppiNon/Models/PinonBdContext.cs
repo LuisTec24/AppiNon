5dbb742eaff0ed7be3ee0054b1129f67eb4011fd
MishiiKnight <156273760+MishiiKnight@users.noreply.github.com>
2025-05-28 17:28:14 -0600

    Update en las connectionString
    1- Utilizacion de la bd en somee (no requiere cambiar al SQL local)
    2- Bd actualizada (quite el trigger culero en usuarios)
    3- ConnectionStrings en appsettings cambiada por la somee (punto 1)

--- AppiNon/Models/PinonBdContext.cs
+++ AppiNon/Models/PinonBdContext.cs
@@ -27,7 +27,7 @@ public partial class PinonBdContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => //optionsBuilder.UseSqlServer("Server=LAPTOP-0CSF1UKT; DataBase=PinonBD;Integrated Security=true; TrustServerCertificate=True");
-    optionsBuilder.UseSqlServer("Server=LUIS; DataBase=PinonBD;Integrated Security=true; TrustServerCertificate=True");
+    optionsBuilder.UseSqlServer("workstation id=PinonBD.mssql.somee.com;packet size=4096;user id=MishiKenai_SQLLogin_1;pwd=lvae14eizv;data source=PinonBD.mssql.somee.com;persist security info=False;initial catalog=PinonBD;TrustServerCertificate=True");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Categorias>(entity =>  {
